9a9af3a872f5627b16c9cf9adeed480115057431
STanislav Jendrišák <st.jendrisak@gmail.com>
2023-11-08 15:08:18 +0100

    add about me

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ let headerNavShow = document.getElementsByClassName('header-nav')[0];
 let headerPhone = document.getElementsByClassName('menu-button-phone')[0];
 let headerPhoneContainer = document.getElementsByClassName('header-contact-container')[0];
 let headerPhoneCLoseButton = document.getElementsByClassName('hide-phone')[0];
+let aboutReadMore = document.getElementsByClassName('about-read-more')[0];
+let aboutReadMoreWrapper =
+  document.getElementsByClassName('about-me-wrapper')[0];
 
-  // getElementsByClassName(headerPhoneContainer);
+// console.log(aboutReadMoreWrapper)
 
 navIcon1.addEventListener('click', function () {
   navIcon1.classList.toggle('open');
@@ -20,3 +23,20 @@ headerPhone.addEventListener('click', function () {
 headerPhoneCLoseButton.addEventListener('click', function ()  {
   headerPhoneContainer.classList.remove('header-contact-container-show');
 })
+
+aboutReadMore.addEventListener('click', () => {
+  aboutReadMoreWrapper.classList.toggle('wrapper-content-open');
+} )
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach( entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll('.animated')[0].classList.add('fade-block');
+    }
+  } )
+})
+
+observer.observe(document.querySelector('main'))
+
+const main = document.querySelector('main')
+console.log(main)
