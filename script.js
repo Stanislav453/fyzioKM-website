d73d99bf497a5242ce0faac8dc80d91bdf0d8e4d
STanislav Jendrišák <st.jendrisak@gmail.com>
2023-11-15 21:59:01 +0100

    add price list

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let navIcon1 = document.getElementById('nav-icon1');
 let buttonPhone = document.getElementsByClassName('menu-button-phone')[0];
 let headerNavShow = document.getElementsByClassName('header-nav')[0];
 let headerPhone = document.getElementsByClassName('menu-button-phone')[0];
-let headerPhoneContainer = document.getElementsByClassName('header-contact-container')[0];
+let headerPhoneContainer = document.getElementsByClassName(
+  'header-contact-container'
+)[0];
 let headerPhoneCLoseButton = document.getElementsByClassName('hide-phone')[0];
 let aboutReadMore = document.getElementsByClassName('about-read-more')[0];
 let aboutReadMoreWrapper =
@@ -15,29 +17,42 @@ navIcon1.addEventListener('click', function () {
   headerNavShow.classList.toggle('header-nav-show');
 });
 
-
 headerPhone.addEventListener('click', function () {
   headerPhoneContainer.classList.add('header-contact-container-show');
 });
 
-headerPhoneCLoseButton.addEventListener('click', function ()  {
+headerPhoneCLoseButton.addEventListener('click', function () {
   headerPhoneContainer.classList.remove('header-contact-container-show');
-})
+});
 
 aboutReadMore.addEventListener('click', () => {
   aboutReadMoreWrapper.classList.toggle('wrapper-content-open');
-} )
+});
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach( entry => {
-    if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
       document.querySelectorAll('.animated')[0].classList.add('fade-block');
       document.querySelectorAll('.animated')[1].classList.add('fade-block');
     }
-  } )
-})
+  });
+});
 
 observer.observe(document.querySelector('.about-me-content-container'));
 
-const main = document.querySelector('main')
-console.log(main)
+const priceList = new IntersectionObserver((item) => {
+  item.forEach((entry) => {
+    if (entry.isIntersecting) {
+      document.querySelectorAll('.box')[0].classList.add('fade-block');
+      document.querySelectorAll('.box')[1].classList.add('fade-block');
+      document.querySelectorAll('.box')[2].classList.add('fade-block');
+      document.querySelectorAll('.box')[3].classList.add('fade-block');
+      document.querySelectorAll('.box')[4].classList.add('fade-block');
+      document.querySelectorAll('.box')[5].classList.add('fade-block');
+      document.querySelectorAll('.box')[6].classList.add('fade-block');
+      document.querySelectorAll('.box')[7].classList.add('fade-block');
+    }
+  });
+});
+
+priceList.observe(document.querySelector('.price-list-content'));
