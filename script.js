6b779079482ce4b3e9e1b4f16ba26a411ccc497d
STanislav Jendrišák <st.jendrisak@gmail.com>
2023-11-28 22:05:32 +0100

    add curs section

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ let headerPhoneCLoseButton = document.getElementsByClassName('hide-phone')[0];
 let aboutReadMore = document.getElementsByClassName('about-read-more')[0];
 let aboutReadMoreWrapper =
   document.getElementsByClassName('about-me-wrapper')[0];
+let cursContentContainer = document.querySelectorAll('.curs-content-container');
+let openWrapperB = document.querySelectorAll('.open-wrapper-b');
+let bxsChevronsDown = document.querySelectorAll('.bxs-chevrons-down');
 
+////////////////////////CODE
 navIcon1.addEventListener('click', function () {
   navIcon1.classList.toggle('open');
   headerNavShow.classList.toggle('header-nav-show');
@@ -30,8 +34,8 @@ aboutReadMore.addEventListener('click', () => {
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      document.querySelectorAll('.animated')[0].classList.add('fade-block');
-      document.querySelectorAll('.animated')[1].classList.add('fade-block');
+      document.querySelectorAll('.animated')[0].classList.add('fade-left');
+      document.querySelectorAll('.animated')[1].classList.add('fade-right');
     }
   });
 });
@@ -43,14 +47,66 @@ const priceList = new IntersectionObserver((item) => {
     if (entry.isIntersecting) {
       document.querySelectorAll('.box')[0].classList.add('fade-block');
       document.querySelectorAll('.box')[1].classList.add('fade-block');
+      document.querySelectorAll('.box')[1].style.animationDelay = '.7s';
       document.querySelectorAll('.box')[2].classList.add('fade-block');
+      document.querySelectorAll('.box')[2].style.animationDelay = '.9s';
       document.querySelectorAll('.box')[3].classList.add('fade-block');
+      document.querySelectorAll('.box')[3].style.animationDelay = '1.1s';
       document.querySelectorAll('.box')[4].classList.add('fade-block');
+      document.querySelectorAll('.box')[4].style.animationDelay = '1.3s';
       document.querySelectorAll('.box')[5].classList.add('fade-block');
+      document.querySelectorAll('.box')[5].style.animationDelay = '1.5s';
       document.querySelectorAll('.box')[6].classList.add('fade-block');
+      document.querySelectorAll('.box')[6].style.animationDelay = '1.7s';
       document.querySelectorAll('.box')[7].classList.add('fade-block');
     }
   });
 });
 
 priceList.observe(document.querySelector('.price-list-content'));
+
+// TIMELINE
+
+(function () {
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll('.timeline li');
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add('in-view');
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener('load', callbackFunc);
+  window.addEventListener('resize', callbackFunc);
+  window.addEventListener('scroll', callbackFunc);
+})();
+
+openWrapperB.forEach((item, buttonId) =>
+  item.addEventListener('click', () => {
+    cursContentContainer.forEach((item, itemId) => {
+      if (buttonId === itemId) {
+        item.classList.toggle('curs-content-container-is-open');
+        bxsChevronsDown[itemId].classList.toggle('icon-move');
+      }
+    });
+  })
+);
